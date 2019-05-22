d8a86f84220f80e87ba01ff6b4d2a9d53c8e882b
Jeremy Fowler <jeremy.f76@gmail.com>
2019-05-22 09:36:11 -0500

    commit error 1

--- NetVU/2019 Accelerate - Cincinnati/AIM222 - SQL for the Intermediate-Advanced User/Scripts/1 - QUERY - Search Stored Procedures.sql
+++ NetVU/2019 Accelerate - Cincinnati/AIM222 - SQL for the Intermediate-Advanced User/Scripts/1 - QUERY - Search Stored Procedures.sql
@@ -5,4 +5,5 @@ SELECT DISTINCT o.name AS Object_Name, o.type_desc
 FROM sys.sql_modules m 
 INNER JOIN sys.objects o 
 ON m.object_id=o.object_id
-WHERE m.definition Like '%Error 02 - Could Not Create AP records for payment transactions%'
+WHERE m.definition Like '%Error 01 - Could Not Create AP records for payment transactions%'
+
